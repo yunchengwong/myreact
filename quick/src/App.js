da42674b80a1296fb7f5f2cc85efffe2e22e4b9c
yunchengwong <yuncheng.wong@hotmail.com>
2023-07-27 15:06:38 +0800

    displaying data

--- quick/src/App.js
+++ quick/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MyButton from './components/MyButton';
 import AboutPage from './components/AboutPage';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <MyButton />
       <AboutPage />
       <img className="avatar" src={logo} />
+      <Profile />
     </div>
   );
 }
