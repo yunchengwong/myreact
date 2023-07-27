9a6f6a3e3f7e8062cc59ba377861ad444653f35b
yunchengwong <yuncheng.wong@hotmail.com>
2023-07-27 14:49:34 +0800

    writing markup with jsx

--- quick/src/App.js
+++ quick/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MyButton from './components/MyButton';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my app</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
---
+++ quick/src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  )
+}
+
+export default AboutPage
