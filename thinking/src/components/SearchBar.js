2d9f8dee9bbc18ccdf826bcd9c210b7a7511bb12
yunchengwong <yuncheng.wong@hotmail.com>
2023-07-28 15:18:30 +0800

    static version

---
+++ thinking/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <form>
+      <input type='text' placeholder='Search' />
+      <label>
+        <input type='checkbox' />
+        Only show products in stock
+      </label>
+    </form>
+  )
+}
+
+export default SearchBar
